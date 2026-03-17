82fd6b419d97379bf7462d177b3ec45bf82158e7
zalmosawi <zalmosawii65@gmail.com>
2026-03-17 03:15:10 +0300

    add the PlayerMovement

---
+++ Assets/_3DStealthGame/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public InputAction MoveAction;
+
+    public float walkSpeed = 1.0f;
+    public float turnSpeed = 20f;
+
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start ()
+    {
+        m_Rigidbody = GetComponent<Rigidbody> ();
+        MoveAction.Enable();
+    }
+
+    void FixedUpdate ()
+    {
+        var pos = MoveAction.ReadValue<Vector2>();
+        
+        float horizontal = pos.x;
+        float vertical = pos.y;
+        
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
+
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation (desiredForward);
+        
+        m_Rigidbody.MoveRotation (m_Rotation);
+        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * walkSpeed * Time.deltaTime);
+    }
+}
